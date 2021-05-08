6fb03f73050a7995841baa77e69e722a9de69d2a
Cesar De La Portilla <cesar.dlp135@gmail.com>
2021-05-07 21:20:01 -0500

    adding files

---
+++ index.js
@@ -0,0 +1,10 @@
+// user is gonn run 'node index.js'
+
+// inquirer will ask questions and user will create employee objects
+
+// once user is done create a js file inside of dist that has an array of employee objects
+
+// import the file with the array into the html before script.js
+
+// use scripts.js to write dom manipulation and create card elements for the html
+
