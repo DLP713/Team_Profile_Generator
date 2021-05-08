1edba9f3ee9c09401b48b540c5cd32b3a3874720
Cesar De La Portilla <cesar.dlp135@gmail.com>
2021-05-07 22:18:25 -0500

    editing indexjs

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+
+
 // user is gonn run 'node index.js'
 
 // inquirer will ask questions and user will create employee objects
@@ -7,4 +16,5 @@
 // import the file with the array into the html before script.js
 
 // use scripts.js to write dom manipulation and create card elements for the html
+
 
